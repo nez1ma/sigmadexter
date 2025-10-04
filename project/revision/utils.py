3e5553dc360e0250c0bc40180f55f0714417aac4
nez1ma <nez703954@gmail.com>
2025-10-04 18:55:51 +0200

    HW7

--- project/revision/utils.py
+++ project/revision/utils.py
@@ -1,12 +1,18 @@
 from typing import List
 import requests
 import json
+from datetime import datetime
 
 SOURCE_TOKEN = "dUcpSF6YYLDjP7h3zDepDNip"
 HOST = "https://s1523333.eu-nbg-2.betterstackdata.com"
 
+
 def send_log(message: str):
-    payload = {"message": message}
+    """Отправка логов на сервер"""
+    payload = {
+        "dt": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC"),
+        "message": message
+    }
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {SOURCE_TOKEN}"
@@ -18,25 +24,33 @@ def send_log(message: str):
     except Exception as e:
         print("Failed to send log:", e)
 
+
 def greet_person(name: str = "Гість") -> str:
     result = f"Привіт, {name}!"
     send_log(f"greet_person({name}) -> {result}")
     return result
+
 
 def is_even(number: int) -> bool:
     result = number % 2 == 0
     send_log(f"is_even({number}) -> {result}")
     return result
 
+
 def reverse_string(text: str) -> str:
     result = text[::-1]
     send_log(f"reverse_string({text}) -> {result}")
     return result
 
+
 def calculate_average(numbers: List[float]) -> float:
-    result = sum(numbers) / len(numbers) if numbers else 0.0
+    if not numbers:
+        result = 0.0
+    else:
+        result = sum(numbers) / len(numbers)
     send_log(f"calculate_average({numbers}) -> {result}")
     return result
+
 
 def add_person_to_list(people: List[str], person: str) -> List[str]:
     new_list = people.copy()
@@ -44,11 +58,13 @@ def add_person_to_list(people: List[str], person: str) -> List[str]:
     send_log(f"add_person_to_list({people}, {person}) -> {new_list}")
     return new_list
 
+
 def count_vowels(text: str) -> int:
     vowels = "aeiouаеєиіїоуюя"
     result = sum(1 for ch in text.lower() if ch in vowels)
     send_log(f"count_vowels({text}) -> {result}")
     return result
+
 
 def fahrenheit_to_celsius(f: float) -> float:
     result = (f - 32) * 5.0 / 9.0
